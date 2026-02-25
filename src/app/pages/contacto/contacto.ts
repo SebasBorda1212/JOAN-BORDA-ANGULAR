b53f54dfd75e0049e37f2f4de67cadd66d548d71
SebasBorda1212 <johansebastianborda@gmail.com>
2026-02-25 16:15:57 -0500

    Actualización

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
 export class Contacto {
+
+  guardar(formData: any) {
+
+    const datosJSON = JSON.stringify(formData, null, 2);
+
+    const blob = new Blob([datosJSON], { type: 'application/json' });
+
+    const url = window.URL.createObjectURL(blob);
+
+    const enlace = document.createElement('a');
+    enlace.href = url;
+    enlace.download = 'contacto.json';
+    enlace.click();
+
+    window.URL.revokeObjectURL(url);
+  }
 
 }
