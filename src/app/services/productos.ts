5994a5abe8cb26f58c723ad383b7406882e02b03
SebasBorda1212 <johansebastianborda@gmail.com>
2026-02-26 16:38:31 -0500

    ActualizaciónPruebafin

--- src/app/services/productos.ts
+++ src/app/services/productos.ts
@@ -10,21 +10,21 @@ export class ProductosService {
       {
         nombre: 'Plan Básico',
         precio: 800000,
-        meses: 48,
+        meses: 5,
         destacado: false,
         imagen: 'basico.jpg'
       },
       {
         nombre: 'Plan Premium',
         precio: 1200000,
-        meses: 36,
+        meses: 8,
         destacado: true,
         imagen: 'premium.jpg'
       },
       {
         nombre: 'Plan Elite',
         precio: 1800000,
-        meses: 24,
+        meses: "Sin limite de",
         destacado: false,
         imagen: 'elite.jpg'
       }
